feb5fb1ded1e8a8d8124085c9161b414ec27d565
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-03-26 22:32:53 -0500

    fix: set useUnifiedTopology: true

--- src/config/db/mongo.ts
+++ src/config/db/mongo.ts
@@ -2,6 +2,7 @@ import { MongoClient } from 'mongodb'
 
 export function getMongoClient() {
   return new MongoClient(process.env.MONGO_URL, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
 }
