6d82f4e6511253a0a84c2a33b66b15c709cffb62
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-03-26 22:23:22 -0500

    chore: add --noEmit to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,6 @@ module.exports = {
   '*.{js,json,md,xml,yml}': 'prettier --ignore-path .gitignore --write',
   '*.js': [
     'eslint --ignore-path .gitignore --fix',
-    () => 'tsc -p tsconfig.json'
+    () => 'tsc -p tsconfig.json --noEmit'
   ]
 }
