253dc8cd31288cac97fb13c643b694bb6056d6cc
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-03-28 17:36:27 -0500

    test: use dist as root folder

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
-const fs = require('fs')
-
-const root = fs.existsSync('dist') ? 'dist' : 'src'
-
 module.exports = {
   verbose: true,
-  roots: [`<rootDir>/${root}`]
+  roots: ['<rootDir>/dist']
 }
