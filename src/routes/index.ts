8fe4cb1670ef083a9bca3989a2a061524f06b700
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-03-28 20:08:21 -0500

    fix: add new routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,10 @@
 import { Server } from 'restify'
 import placeRoutes from './place'
+import jobRoutes from './job'
+import statisticRoutes from './statistic'
 
 export default (server: Server) => {
   placeRoutes(server)
+  jobRoutes(server)
+  statisticRoutes(server)
 }
